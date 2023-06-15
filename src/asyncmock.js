806e6822e238adc59bdefdcf1b0273dc9f9f04d5
Juan Martino <martinojuan10@gmail.com>
2023-06-15 12:33:01 -0300

    imagenes

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,8 +1,8 @@
 const productos = [
-    { id: 1, nombre: "Yerba", precio: 500, img: "./img/yerba.jpg" },
-    { id: 2, nombre: "Azucar", precio: 200, img: "./img/azucar.jpg" },
-    { id: 3, nombre: "Aceite", precio: 400, img: "./img/aceite.jpg" },
-    { id: 4, nombre: "Cafe", precio: 600, img: "./img/cafe.jpg" },
+    { id: 1, nombre: "Yerba", precio: 500, img: "./Img/yerba.png" },
+    { id: 2, nombre: "Azucar", precio: 200, img: "./Img/azucar.png" },
+    { id: 3, nombre: "Aceite", precio: 400, img: "./Img/aceite.png" },
+    { id: 4, nombre: "Cafe", precio: 600, img: "./Img/cafe.png" },
 ]
 
 export const getProductos = () => {
